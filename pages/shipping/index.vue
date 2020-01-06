df11ada0ef98bdf515b6558a4e1d809fc7db1637
zhanghongi <742963285@qq.com>
2020-01-06 16:11:35 +0800

    商品主页文件创建

---
+++ pages/shipping/index.vue
@@ -0,0 +1,29 @@
+<template>
+	<view>
+		<view class="header">
+			<text>商品主页</text>
+			<view>
+				<text class="text">测试sass</text>
+			</view>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			};
+		}
+	}
+</script>
+
+<style lang="scss">
+.header{
+	color: #007AFF;
+	.text{
+		color: red;
+	}
+}
+</style>
